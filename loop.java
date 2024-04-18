7c1bd8a82a722bcbc6da830a8d0ad5b25efb6fc1
shreya6900 <51473691+shreya6900@users.noreply.github.com>
2024-04-18 23:18:13 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,28 @@
+package com.loop;
+
+public class loop
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<=100;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(" "+i);
+			}
+		}
+	   int j=0;
+	   while(j<=100)
+	   {
+		   if(j%2!=0)
+		   {
+			   System.out.print(" "+j); 
+			   j++;
+		   }
+		   
+	   }
+	}
+}
+		
+	
